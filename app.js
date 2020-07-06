58136a6641e8eca6607ef8943683ac7cf79772b5
swift1719 <ayushpawar1501@gmail.com>
2020-07-06 20:21:33 +0530

    1.configured app

---
+++ app.js
@@ -0,0 +1,31 @@
+let bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	express = require('express'),
+	app = express();
+//App Config
+mongoose.connect('mongodb://localhost:27017/blogapp', { useNewUrlParser: true, useUnifiedTopology: true });
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+//Mongoose/Model config
+let blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: { type: Date, default: Date.now() }
+});
+
+let blog = mongoose.model('blogs', blogSchema);
+
+blog.create({
+	title: 'Test Blog',
+	image:
+		'https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+	body: "what's new is all here.."
+});
+
+//RESTful Routes
+
+app.listen(3000, () => {
+	console.log('Server Started!');
+});
