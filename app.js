8ce96e406a317175ba7acf75d4c7103c36512832
swift1719 <ayushpawar1501@gmail.com>
2020-07-07 10:07:46 +0530

    2. layout set up

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let bodyParser = require('body-parser'),
 //App Config
 mongoose.connect('mongodb://localhost:27017/blogapp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.set('view engine', 'ejs');
-app.set(express.static('public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 //Mongoose/Model config
 let blogSchema = new mongoose.Schema({
@@ -17,14 +17,26 @@ let blogSchema = new mongoose.Schema({
 
 let blog = mongoose.model('blogs', blogSchema);
 
-blog.create({
-	title: 'Test Blog',
-	image:
-		'https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
-	body: "what's new is all here.."
-});
+// blog.create({
+// 	title: 'Test Blog',
+// 	image:
+// 		'https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+// 	body: "what's new is all here.."
+// });
 
 //RESTful Routes
+app.get('/', (req, res) => {
+	res.redirect('/blogs');
+});
+app.get('/blogs', (req, res) => {
+	blog.find({}, (err, allblogs) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('index', { blogs: allblogs });
+		}
+	});
+});
 
 app.listen(3000, () => {
 	console.log('Server Started!');
